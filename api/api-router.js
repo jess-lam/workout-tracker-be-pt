86231a508df376398863954b5773be021a032987
jennl97 <jennhott97@gmail.com>
2020-03-11 21:26:13 -0500

    try again

--- api/api-router.js
+++ api/api-router.js
@@ -2,29 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userAccess = require('../databaseAccess/userAccess')
 
-// router.get('/', (req, res) => {
-//     userAccess.getUsers()
-//     .then((users) => {
-//         res.status(200);
-//         res.send(users);
-//     })
-//     .catch((err) => {
-//         res.status(500);
-//         res.send(err);
-//     })
-// })
-
 router.get('/db', (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+    userAccess.getUsers()
+    .then((test_table) => {
+        res.status(200);
+        res.send(users);
+    })
+    .catch((err) => {
+        res.status(500);
+        res.send(err);
+    })
+})
+
 
 module.exports = router;
