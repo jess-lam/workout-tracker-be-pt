2066ca3f76fe3a06968b45363ab201f9551c9e90
jennl97 <jennhott97@gmail.com>
2020-03-11 21:10:03 -0500

    updated db call

--- api/api-router.js
+++ api/api-router.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     })
 })
 
-.get('/db', async (req, res) => {
+router.get('/db', (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
