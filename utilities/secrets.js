0b2eaaca8f036a3bd524b61ebe8dee66597c0543
Nathan <nate1898@gmail.com>
2020-05-28 00:16:53 -0400

    Update secrets.js (#16)

--- utilities/secrets.js
+++ utilities/secrets.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     jwtSecret: JSON.parse(`"${process.env.JWT_SECRET}"`) || "Peanut_Butter_Jelly_Time",
-    jwtPublic: JSON.parse(`"${process.env.JWT_Public}"`) || "Test"
+    jwtPublic: JSON.parse(`"${process.env.JWT_PUBLIC}"`) || "Test"
 };
