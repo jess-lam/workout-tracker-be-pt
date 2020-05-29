d81099d6846b19bf58d74dd2d43f45487f6344b6
Kristi Gribble <kgribble4083@gmail.com>
2020-05-29 12:13:38 -0500

    Added tests to user.spec.js testing registration and login functionality

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
-const knex = require("knex");
+const knex = require('knex');
 const config = require('../knexfile');
-const environment = process.env.NODE_ENV || 'development';
-
+const environment = process.env.DB_ENV || 'development';
+console.log(process.env.DB_ENV);
 module.exports = knex(config[environment]);
