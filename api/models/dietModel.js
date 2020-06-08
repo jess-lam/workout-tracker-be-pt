667dffc50c92090219adfae8fbe59847994cb932
Kristi Gribble <kgribble4083@gmail.com>
2020-06-08 08:03:47 -0500

    fixed getAll query to return the correct information

--- api/models/dietModel.js
+++ api/models/dietModel.js
@@ -18,23 +18,24 @@ function findById(id) {
 
 function getAll(id) {
   return db('diets')
-  .join('users', 'users.id', '=', 'diets.user_id')
-  .where('diets.user_id','=', id)
-  .select(
-    'diets.id',
-    'diets.user_id',
-    'diets.meal_date',
-    'diets.meal_time',
-    'diets.meal_category',
-    'diets.food_name',
-    'diets.food_quantity',
-    'diets.food_measure',
-    'diets.food_calories',
-    'diets.food_fat',
-    'diets.food_protein',
-    'diets.food_carbs',
-    'diets.food_fiber'
-    )
+    .join('users', 'users.id', 'diets.user_id')
+    .select( 
+      'diets.id',
+      'diets.user_id',
+      'diets.meal_date',
+      'diets.meal_time',
+      'diets.meal_category',
+      'diets.food_name',
+      'diets.food_quantity',
+      'diets.food_measure',
+      'diets.food_calories',
+      'diets.food_fat',
+      'diets.food_protein',
+      'diets.food_carbs',
+      'diets.food_fiber'
+      )
+    .from('diets')
+    .where('diets.user_id', id)
 }
 
 function update(changes, id) {
