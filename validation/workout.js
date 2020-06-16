6c5361f867465cb0b1c5330e9d5b0a6bf5e1224f
Kristi <kgribble4083@gmail.com>
2020-06-15 22:40:19 -0500

    Revert "Hopefully fixes codeclimate grade"

--- validation/workout.js
+++ validation/workout.js
@@ -8,6 +8,16 @@ module.exports = function checkRegistrationFields(workout) {
     //catches errors
     let error = {};
 
+    /*expected data 
+        workout_category
+        workout_title
+        workout_date
+        workout_start_time
+        workout_end_time
+        workout_description
+        completed
+    */
+
     let data = {
         ...workout
     }
@@ -18,15 +28,15 @@ module.exports = function checkRegistrationFields(workout) {
     data.workout_length = ifEmpty(data.workout_length) ? data.workout_length : "";
     data.workout_description = ifEmpty(data.workout_description) ? data.workout_description : "";
 
-    if (data.workout_category)
+    if (data.workout_category = "")
         error.workout_category = "Category is required";
-    if (data.workout_title)
+    if (data.workout_title = "")
         error.workout_title = "Title is required"
     if (!checkDate.test(data.workout_date))
-        error.workout_dateaaaa = "date is invalid"
+        error.workout_date = "date is invalid"
     if (!checkTime.test(data.workout_length))
         error.workout_length = "Length is invalid should follow ##m or ##h standard"
-    if (data.workout_description)
+    if (data.workout_description = "")
         error.workout_description = "Workout Description is invalid"
 
     return {
