5448cfbc8fe8bea678d66996bf53583071d2ae7b
Nathan <nate1898@gmail.com>
2020-06-16 22:59:48 -0400

    Fixes some of the duplication issues from codeclimate (#33)

--- validation/checkForEmpty.js
+++ validation/checkForEmpty.js
@@ -1,12 +1,8 @@
 const ifEmpty = field => {
     try {
-        let results = true;
-        if( 
-            field === undefined || field === null || 
-            (typeof field === "string" && field.trim().length === 0) || 
-            (typeof field === "object" && Object.keys(field).length === 0)
-        )
-        results = false;      
+        let results = false;
+        if(field !== undefined || field !== null )
+        results = true;      
         return results;  
     } catch(error){
         return error;
