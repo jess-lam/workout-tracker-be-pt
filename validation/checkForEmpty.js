9d61cfe217baa28117bc63f744d0094c3a700979
Nathan <nate1898@gmail.com>
2020-06-16 23:18:42 -0400

    Check if empty valid fixing

--- validation/checkForEmpty.js
+++ validation/checkForEmpty.js
@@ -1,8 +1,13 @@
 const ifEmpty = field => {
     try {
-        let results = false;
-        if(field !== undefined || field !== null )
-        results = true;      
+        let results = true;
+        if( field === undefined || field === null || 
+        (typeof field === "string" && field.trim().length === 0) || 
+        (typeof field === "object" && Object.keys(field).length === 0) )
+        {
+                results = false;      
+        }
+
         return results;  
     } catch(error){
         return error;
