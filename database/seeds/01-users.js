30c821835607683b27ba7aa24cfa6e5627462e66
Kristi Gribble <kgribble4083@gmail.com>
2020-06-19 21:41:53 -0500

    Added fields to seeds files since table has changed.

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -6,9 +6,39 @@ function encrypt(string){
 
 exports.seed = function(knex, Promise) {
       return knex('users').insert([
-        {username: 'test', email: 'test@gmail.com', password: encrypt('password'), affiliate: false, verified: false, xp: 0},
-        {username: 'nate', email: 'nate1898@gmail.com', password: encrypt('password'), affiliate: false, verified: false, xp: 0},
-        {username: 'cory', email: 'cory@gmail.com', password: encrypt('password'), affiliate: false, verified: false, xp: 0}
+        {username: 'test',
+         email: 'test@gmail.com',
+         password: encrypt('password'),
+         bio: "Full Stack Web Developer",
+         zip: 53546,
+         affiliate: false,
+         verified: false,
+         xp: 0,
+         height: 65,
+         weight: 150,
+        },
+        {username: 'nate',
+         email: 'nate1898@gmail.com',
+         password: encrypt('password'),
+         bio: "Full Stack Web Developer, Amateur Chef",
+         zip: 16424,
+         affiliate: false,
+         verified: false,
+         xp: 0,
+         height: 72,
+         weight: 240,        
+        },
+        {username: 'cory',
+        email: 'cory@gmail.com',
+        password: encrypt('password'),
+        bio: "Front End Web Developer",
+        zip: 55698,
+        affiliate: false,
+        verified: false,
+        xp: 0,
+        height: 75,
+        weight: 200
+        } 
       ]);
    };
 
