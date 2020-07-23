c9d6ffc10757cb74c073578b1dbd3d15795e109b
Nathan <nate1898@gmail.com>
2020-07-23 17:36:50 -0400

    Feature/add user data (#55)
    * Adds The ability to pull all entity items that the connected components are marked as shareable
    
    * Update README.md
    
    * Adds user data
    
    Adds the user data when getting a list of entities for the feed that way front end doesn't need to make another call cause thats silly.

--- api/models/entityModel.js
+++ api/models/entityModel.js
@@ -11,25 +11,26 @@ const types = {
 async function getPublic(){
     const list = [];
     await Promise.all((await db('entity').orderBy('entity.created_at', 'desc')).map(async entity =>{
+        let [user] = await db('users').where('id', '=', entity.user_id).select('users.id', 'users.username', 'users.image')
         switch(entity.type){
             case types.WORKOUT: 
                     let [workout] = await db('workouts').where('workouts.entity_id', '=', entity.id).where('workouts.workout_share', '=', true);
-                    workout = {...workout, type: types.WORKOUT}
+                    workout = {...workout, type: types.WORKOUT, user: user}
                     list.push(workout);
                 break;
             case types.ROUTINE: 
                     let [routine] = await db('routines').where('routines.entity_id', '=', entity.id).where('routines.shareable', '=', true);
-                    routine = {...routine, type: types.ROUTINE}
+                    routine = {...routine, type: types.ROUTINE, user: user}
                     list.push(routine);
                 break;
             case types.DIET: 
                     let [diet] = await db('diets').where('diets.entity_id', '=', entity.id).where('diets.shareable', '=', true);
-                    diet = {...diet, type: types.DIET} 
+                    diet = {...diet, type: types.DIET, user: user} 
                     list.push(diet);
                 break;
             case types.MEALPLAN: 
                     let [mealplan] = await db('mealplans').where('mealplans.entity_id', '=', entity.id).where('mealplans.shareable', '=', true);
-                    mealplan = {...mealplan, type: types.MEALPLAN}
+                    mealplan = {...mealplan, type: types.MEALPLAN, user: user}
                     list.push(mealplan);
                 break;
             case types.COMMENT:
