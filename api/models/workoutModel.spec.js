7ad33d03168ed383ba80ff7edfee5060d11a2b9c
Nathan <nate1898@gmail.com>
2020-08-25 16:54:47 -0400

    Create workoutModel.spec.js

---
+++ api/models/workoutModel.spec.js
@@ -0,0 +1,10 @@
+const { request } = require('express');
+
+const {getPublicWorkouts} = require('./workoutModel');
+
+describe('Workout Function test', () => {
+    it('should get public data', () => {
+        const data = getPublicWorkouts();
+        expect(data).not.toBe(null);
+    });
+});
